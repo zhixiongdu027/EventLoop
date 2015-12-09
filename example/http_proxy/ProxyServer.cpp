7efbb1c604a558bfafe34c021f9fc45365d71dde
adugeek <adugeek@gmail.com>
2015-12-09 17:26:56 +0800

    nothing

--- example/http_proxy/ProxyServer.cpp
+++ example/http_proxy/ProxyServer.cpp
@@ -1,12 +1,13 @@
 //
 // Created by adugeek on 15-7-9.
 //
+#include <iostream>
+#include <EventLoop/Forward.h>
 #include "ProxyServer.h"
 #include "EventLoop/tool/SocketHelp.h"
 
 void ProxyServer::proxy_server_forward_cb(EventLoopPtr &loop_ptr, ChannelPtr &channel_ptr, ChannelId events) {
-    ChannelContext *channel_context = static_cast<ChannelContext *>(channel_ptr->context.ptr);
-    ChannelPtr &client_ptr = loop_ptr->get_channel(channel_context->other_side);
+    ChannelPtr &client_ptr = loop_ptr->get_channel(channel_ptr->context.u32);
     if (client_ptr != nullptr && events == EVENT_IN && channel_ptr->read() > 0) {
         loop_ptr->add_channel_lifetime(channel_ptr->id(), 60);
         StreamBuffer *buffer = channel_ptr->get_read_buffer();
@@ -14,7 +15,7 @@ void ProxyServer::proxy_server_forward_cb(EventLoopPtr &loop_ptr, ChannelPtr &ch
         buffer->discard_all();
     }
     else {
-        loop_ptr->erase_channel(channel_context->other_side);
+        loop_ptr->erase_channel(channel_ptr->context.u32);
         loop_ptr->erase_channel(channel_ptr->id());
     }
 }
@@ -50,15 +51,14 @@ void ProxyServer::proxy_client_connect_cb(EventLoopPtr &loop_ptr, ChannelPtr &ch
             auto &head_info = channel_context->http_parser.head_info;
             if (head_info.find("HOST") == head_info.end()) { break; }
             std::string remote_host = std::string(buffer->peek(head_info["HOST"].first), head_info["HOST"].second);
-
             if (remote_host == "") { break; }
             int remote_sock;
-            if (tcp_nonblock_connect(remote_host.c_str(), 80, &remote_sock) ==
-                NONBLOCK_CONNECT_ERROR) { break; };
+            if (tcp_nonblock_connect(remote_host.c_str(), 80, &remote_sock) == NONBLOCK_CONNECT_ERROR) { break; };
 
             ChannelPtr &remote_ptr = loop_ptr->add_connecting_channel(remote_sock, 5, 60, proxy_server_forward_cb);
             if (remote_ptr == nullptr) { break; }
-            remote_ptr->get_write_buffer()->swap(*buffer);
+            remote_ptr->send(buffer->peek(), buffer->peek_able());
+            buffer->discard_all();
             remote_ptr->context.u32 = channel_ptr->id();
             channel_context->http_parser.clear();
             channel_context->other_side = remote_ptr->id();
@@ -72,9 +72,14 @@ void ProxyServer::proxy_client_connect_cb(EventLoopPtr &loop_ptr, ChannelPtr &ch
 void ProxyServer::proxy_listen_cb(EventLoopPtr &loop_ptr, ChannelPtr &channel_ptr, ChannelId events) {
     if (events == EVENT_IN) {
         int nfd = accept4(channel_ptr->fd(), nullptr, nullptr, SOCK_NONBLOCK);
-        if (loop_ptr->add_channel(nfd, true, true, 60, proxy_client_connect_cb) == nullptr) {
+        ChannelPtr &new_channel = loop_ptr->add_channel(nfd, true, true, 60, proxy_client_connect_cb);
+        if (new_channel == nullptr) {
             close(nfd);
+            return;
         }
+        ChannelContext *new_context = new ChannelContext;
+        new_channel->context.ptr = new_context;
+        new_channel->context_deleter = [](void *rhs) { delete static_cast<ChannelContext *>(rhs); };
     }
     else {
         loop_ptr->stop();
