52fae713ae6226a2646aef6756d3dc2d601c1c31
adugeek <adugeek@gmail.com>
2015-12-16 14:58:52 +0800

    nothing

--- example/task/main.cpp
+++ example/task/main.cpp
@@ -8,7 +8,7 @@
 int main() {
     EventLoop loop;
     loop.add_task_on_loop(true, 5, nullptr, [](EventLoopPtr &, void *, bool *again) {
-        std::cout << "I am here " << std::endl;
+        std::cout << "I am here , See you after 5 seconds  @`_`@  ！" << std::endl;
         *again = true;
     });
     loop.start();
