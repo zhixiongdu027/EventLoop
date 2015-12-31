5be65af488f1c14b690e0c99cfa0abad8ba47abe
adugeek <adugeek@gmail.com>
2015-12-31 17:19:50 +0800

    nothing

--- example/singleton/main.cpp
+++ example/singleton/main.cpp
@@ -3,11 +3,11 @@
 
 class A {
 public:
-    A(int a, int b) : a_(a), b_(b) {};
+    A() : a_(0), b_(0) { };
 
-    A() : a_(1), b_(1) { };
+    A(int a) : a_(a), b_(0) { };
 
-    A(int a) : a_(a), b_() { };
+    A(int a, int b) : a_(a), b_(b) { };
 
     void print() {
         printf("a : %d ,b: %d\n", a_, b_);
@@ -29,7 +29,15 @@ int main() {
     std::unique_ptr<A> &ptr2 = Singleton<A>::get_instance(1);
     ptr2->print();
 
-    std::unique_ptr<A> &ptr3 = Singleton<A>::get_instance(1, 2);
+    std::unique_ptr<A> &ptr3 = Singleton<A>::get_instance(2, 1);
     ptr3->print();
 
+    std::unique_ptr<A> &ptr4 = Singleton<A>::get_instance(2, 2);
+    ptr4->print();
+
+    std::unique_ptr<A> &ptr5 = Singleton<A>::get_instance(2, 3);
+    ptr5->print();
+
+    std::unique_ptr<A> &ptr6 = Singleton<A>::get_instance(2, 4);
+    ptr6->print();
 }
