5a627cec5b71247092c01b8f7289b9cca22ee81f
adugeek <adugeek@gmail.com>
2016-01-13 12:40:34 +0800

    nothing

--- src/EventLoop/tool/Singleton.hpp
+++ src/EventLoop/tool/Singleton.hpp
@@ -27,6 +27,7 @@ public:
                 singleton_.swap(temp);
             }
         }, std::forward<Args>(args)...);
+        return singleton_ == nullptr;
     }
 
     static std::unique_ptr<T> &instance() {
