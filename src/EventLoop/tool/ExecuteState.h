72e3ce03ead83af104e23a10e9575522e6be1a6d
adugeek <adugeek@gmail.com>
2016-01-19 07:22:33 +0800

    nothing

---
+++ src/EventLoop/tool/ExecuteState.h
@@ -0,0 +1,14 @@
+//
+// Created by adugeek on 1/19/16.
+//
+
+#ifndef EVENTLOOP_TOOL_EXECUTESTATE_H
+#define EVENTLOOP_TOOL_EXECUTESTATE_H
+
+enum ExecuteState {
+    ExecuteError = -1,
+    ExecuteProcessing = 0,
+    ExecuteDone = 1
+};
+
+#endif //EVENTLOOP_TOOL_EXECUTESTATE_H
