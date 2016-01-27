ebb54bb8394b26b11f4f31c0efc5b5e20593fc73
adugeek <adugeek@gmail.com>
2016-01-27 09:40:25 +0800

    nothing

--- example/msgpack/main.cpp
+++ example/msgpack/main.cpp
@@ -5,6 +5,15 @@
 #include "EventLoop/tool/BlockingQueue.hpp"
 #include "EventLoop/tool/ThreadPool.h"
 
+struct A {
+    A() : a(std::rand()), b(std::rand()), c(std::rand()) { };
+    int a;
+    int b;
+    int c;
+    MSGPACK_DEFINE (a, b, c);
+};
+
+
 struct Message {
 
     Message(uint32_t type, msgpack::unpacked &&unpacked) : type(type), unpacked(std::move(unpacked)) { };
@@ -50,25 +59,63 @@ int main() {
 
     int listen_sock = create_tcp_listen(10000, 1);
 
-    ThreadPool pool;
-    pool.emplace_back([]() {
-        while (true) {
-            std::unique_ptr<Message> ptr = std::move(queue.pop());
-            msgpack::object object = ptr->unpacked.get();
-            std::cout << "queue: " << "type :" << ptr->type << " obj " << object << std::endl;
-        }
-    });
-
-    pool.detach_all();
-
-    loop.add_channel(listen_sock, true, true, -1,
-                     [](EventLoopPtr &loop_ptr, ChannelPtr &channel_ptr, ChannelEvent event) {
+    loop.add_channel(listen_sock,
+                     true, true, -1,
+                     [](
+                             EventLoopPtr &loop_ptr, ChannelPtr
+                     &channel_ptr,
+                             ChannelEvent event
+                     ) {
                          if (event != EVENT_IN) {
-                             loop_ptr->stop();
+                             loop_ptr->
+
+                                     stop();
+
                              return;
                          }
                          int client_fd = accept(channel_ptr->fd(), nullptr, nullptr);
-                         loop_ptr->add_channel(client_fd, true, false, 100, io_cb);
+                         loop_ptr->
+                                 add_channel(client_fd,
+                                             true, false, 100, io_cb);
                      });
+
+    ThreadPool pool;
+    //send thread;
+    pool.emplace_back([]() {
+        EventLoop send_loop;
+        int socket_fd = tcp_connect("127.0.0.1", 10000);
+        ChannelPtr &channel = send_loop.add_channel(socket_fd, true, false, -1,
+                                                    [](EventLoopPtr &loop_ptr, ChannelPtr &channel_ptr,
+                                                       ChannelEvent event) {
+                                                        loop_ptr->erase_channel(channel_ptr->id());
+                                                    });
+        msgpack::sbuffer buffer;
+        for (int i = 0; i < 100000; ++i) {
+            A temp;
+            buffer.clear();
+            msgpack::pack(buffer, temp);
+            channel->send_block_data(i, buffer.data(), buffer.size());
+        }
+        std::cout << "send thread will exit" << std::endl;
+    }
+    );
+
+    //handle thread
+    pool.emplace_back([]() {
+        while (true) {
+            std::unique_ptr<Message> ptr;
+            if (queue.pop_wait_for(std::chrono::seconds(3), &ptr)) {
+                msgpack::object object = ptr->unpacked.get();
+                std::cout << "queue: " << "type :" << ptr->type << " obj " << object <<
+                std::endl;
+            }
+            else {
+                std::cout << "handle thread will exit" << std::endl;
+                break;
+            }
+        }
+    });
+
     loop.start();
+
 }
