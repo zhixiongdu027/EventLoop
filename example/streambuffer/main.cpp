125e293538489aaae96f9dd15a531df25f4ae42b
adugeek <adugeek@gmail.com>
2016-01-31 21:26:02 +0800

    nothing

--- example/streambuffer/main.cpp
+++ example/streambuffer/main.cpp
@@ -14,7 +14,13 @@ int main() {
     uint64_t z;
     BlockData block3;
 
-    stream_buffer_quick_peek(&buffer, &all, &x, &block1, &y, &block2, &z, &block3);
+    StreamBuffer new_buffer(std::move(buffer));
+
+    StreamBuffer new_new_buffer;
+
+    std::swap(new_new_buffer, new_buffer);
+
+    stream_buffer_quick_peek(&new_new_buffer, &all, &x, &block1, &y, &block2, &z, &block3);
     std::cout << std::string(block1.data, block1.len) << " " << std::string(block2.data, block2.len) << " " <<
     std::string(block3.data, block3.len) << std::endl;
     std::cout << all << " " << x << " " << y << " " << z << " " << std::endl;
